79c6b09a5999d61b9af3eb4c54cad4d512143da7
BELINDA <kennyegun240@gmail.com>
2023-01-06 11:18:04 +0100

    Modified

--- student.rb
+++ student.rb
@@ -1,11 +1,13 @@
 require_relative 'person'
+require_relative 'teacher'
 require_relative 'classroom'
+require_relative 'book'
 
 class Student < Person
   attr_reader :classroom
 
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission)
+    super(name, age, parent_permission: parent_permission)
     @classroom = classroom
   end
 
