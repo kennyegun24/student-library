26eea4da3e45cec3920de19ba405994eb730dea7
BELINDA <kennyegun240@gmail.com>
2023-01-06 11:17:29 +0100

    Modified

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,10 @@
+require_relative 'person'
+
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(specialization)
-    super('Kenny Egun', 21)
+  def initialize(specialization, age, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
