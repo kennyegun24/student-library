9c71d742c414ef9a8859b7434c0298d137cab3e0
BELINDA <kennyegun240@gmail.com>
2023-01-06 11:17:44 +0100

    Modified

--- person.rb
+++ person.rb
@@ -1,39 +1,38 @@
 require_relative 'nameable'
-require_relative 'book'
+require_relative 'student'
 require_relative 'rental'
+require_relative 'book'
 
 class Person < Nameable
+  attr_accessor :name, :age, :rentals
   attr_reader :id
-  attr_accessor :name, :age, :rental
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = rand(1000..9999)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @rental = []
+    @rentals = []
+  end
+
+  def can_use_services?
+    return if of_age? && parent_permission
+
+    true
   end
 
   def correct_name
     @name
   end
 
+  def add_book_rental(book, date)
+    Rental.new(date, book, self)
+  end
+
   private
 
   def of_age?
     @age >= 18
-  end
-
-  public
-
-  def can_use_services?
-    return if of_age? || @parent_permission
-
-    true
-  end
-
-  def add_book_rental(book, date)
-    Rental.new(date, book, self)
   end
 end
